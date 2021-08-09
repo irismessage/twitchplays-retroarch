b40b0ff611de7efb454b5e79879b6b2ce13c0117
JMcB <joel.mcbride1@live.com>
2021-08-09 20:37:38 +0100

    Implement input queue

--- twitchplays_retroarch/__init__.py
+++ twitchplays_retroarch/__init__.py
@@ -1,3 +1,6 @@
+import queue
+import concurrent.futures
+import time
 from pathlib import Path
 
 import pyautogui
@@ -5,8 +8,11 @@ import keyboard
 import toml
 import twitchio.ext.commands
 
+
 # todo: add logging
 # todo: add docstrings
+# todo: customisable command set
+# todo: sync command set with RetroArch config files
 
 
 CONFIG_PATH = Path('config.toml')
@@ -26,13 +32,21 @@ class TwitchPlaysRetroArchBot(twitchio.ext.commands.bot.Bot):
 
     def __init__(
             self,
-            case_sensitive: bool = False, keypress_delay: float = 0.1, keypress_duration: float = 0.1,
-            *args, **kwargs):
+            case_sensitive: bool = False,
+            keypress_delay: float = 0.1, keypress_duration: float = 0.1,
+            input_threads: int = 1,
+            *args, **kwargs
+    ):
         self.case_sensitive = case_sensitive
         self.keypress_delay = keypress_delay
         self.keypress_duration = keypress_duration
         # for pausing user control
         self.twitchplays_commands_enabled = True
+
+        self.input_queue = queue.Queue()
+        self.thread_pool = concurrent.futures.ThreadPoolExecutor(
+            max_workers=input_threads, initializer=self.input_handle_loop
+        )
 
         super().__init__(*args, **kwargs)
 
@@ -48,6 +62,13 @@ class TwitchPlaysRetroArchBot(twitchio.ext.commands.bot.Bot):
             status = 'disabled'
         print(f'Twitch Plays commands {status}.')
 
+    def input_handle_loop(self):
+        while True:
+            if not self.input_queue.empty():
+                key_to_press = self.input_queue.get()
+                pyautogui.press(key_to_press, interval=self.keypress_duration)
+                time.sleep(self.keypress_delay)
+
     async def process_twitchplays_commands(self, message: twitchio.Message) -> bool:
         commandset = self.test_keys_fbneo
 
@@ -56,7 +77,8 @@ class TwitchPlaysRetroArchBot(twitchio.ext.commands.bot.Bot):
             command = command.casefold()
 
         if command in commandset:
-            pyautogui.press(commandset[command], interval=self.keypress_duration)
+            key_to_press = commandset[command]
+            self.input_queue.put(key_to_press)
             return True
 
         return False
