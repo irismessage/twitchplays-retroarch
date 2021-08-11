e365febac5f838685272235f7f2af00fdfffb0a9
JMcB <joel.mcbride1@live.com>
2021-08-11 19:41:42 +0100

    Add full logging

--- twitchplays_retroarch/__init__.py
+++ twitchplays_retroarch/__init__.py
@@ -2,6 +2,8 @@ import queue
 import concurrent.futures
 import threading
 import time
+import sys
+import logging as log
 from pathlib import Path
 
 import pyautogui
@@ -10,20 +12,30 @@ import toml
 import twitchio.ext.commands
 
 
-# todo: add logging
 # todo: add docstrings
 # todo: customisable command set
 # todo: sync command set with RetroArch config files
 # todo: write readme with guide
 # todo: arguments like config location
-# todo: github automation for builds, on release?
 # todo: auto create config.toml from example
 
 
-__version__ = '0.2.0'
+__version__ = '0.3.0'
 
 
 CONFIG_PATH = Path('config.toml')
+
+
+stream_handler = log.StreamHandler(stream=sys.stdout)
+stream_handler.setLevel(log.INFO)
+stream_formatter = log.Formatter('%(message)s')
+stream_handler.setFormatter(stream_formatter)
+log.basicConfig(
+    level=log.DEBUG,
+    handlers=[
+        stream_handler
+    ]
+)
 
 
 class TwitchPlaysRetroArchBot(twitchio.ext.commands.bot.Bot):
@@ -60,7 +72,7 @@ class TwitchPlaysRetroArchBot(twitchio.ext.commands.bot.Bot):
         super().__init__(*args, **kwargs)
 
     async def event_ready(self):
-        print('Connected.')
+        log.info('Bot started.')
 
     def twitchplays_commands_toggle(self):
         self.twitchplays_commands_enabled = not self.twitchplays_commands_enabled
@@ -69,14 +81,14 @@ class TwitchPlaysRetroArchBot(twitchio.ext.commands.bot.Bot):
             status = 'enabled'
         else:
             status = 'disabled'
-        print(f'Twitch Plays commands {status}.')
+        log.info(f'Twitch Plays commands {status}.')
 
     def input_queue_pop(self):
         thread_name = threading.currentThread().name
-        print(f'Handling one input from queue, in thread {thread_name}.')
+        log.info('%s: Handling one input from queue.', thread_name)
 
         key_to_press = self.input_queue.get()
-        print(f'Executing input: {key_to_press}.')
+        log.info('%s: Executing input: %s.', thread_name, key_to_press)
         pyautogui.press(key_to_press, interval=self.keypress_duration)
         time.sleep(self.keypress_delay)
 
@@ -89,7 +101,7 @@ class TwitchPlaysRetroArchBot(twitchio.ext.commands.bot.Bot):
 
         if command in commandset:
             key_to_press = commandset[command]
-            print(f'Queueing input: {key_to_press}.')
+            log.info(f'Queueing input: {key_to_press}.')
             self.input_queue.put(key_to_press)
             self.input_thread_pool.submit(self.input_queue_pop)
             return True
@@ -99,9 +111,10 @@ class TwitchPlaysRetroArchBot(twitchio.ext.commands.bot.Bot):
     async def event_message(self, message: twitchio.Message):
         # ignore messages from the bot
         if message.echo:
+            log.info('Ignoring message from bot: %s', message.content)
             return
 
-        print('Got user message.')
+        log.info('Got user message: %s', message.content)
         if self.twitchplays_commands_enabled:
             await self.process_twitchplays_commands(message)
 
@@ -109,11 +122,13 @@ class TwitchPlaysRetroArchBot(twitchio.ext.commands.bot.Bot):
         await self.handle_commands(message)
 
     async def close(self):
+        log.info('Shutting down bot.')
         self.input_thread_pool.shutdown()
         await super().close()
 
 
 def main():
+    log.info('Loading config.')
     with open(CONFIG_PATH) as config_file:
         config = toml.load(config_file)
 
@@ -130,4 +145,5 @@ def main():
         suppress=True
     )
 
+    log.info('Starting bot.')
     bot.run()
